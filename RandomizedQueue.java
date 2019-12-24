e4709cd116d3d8735e28cefedd0d4f0a399dd8f0
MrMalfunction <007bohora@gmail.com>
2019-12-24 11:26:22 +0530

    Try2

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -4,22 +4,32 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
 
     // is the randomized queue empty?
-    public boolean isEmpty(){}
+    public boolean isEmpty(){
+        return true;
+    }
 
     // return the number of items on the randomized queue
-    public int size(){}
+    public int size(){
+        return 1;
+    }
 
     // add the item
     public void enqueue(Item item){}
 
     // remove and return a random item
-    public Item dequeue(){}
+    public Item dequeue(){
+        return null;
+    }
 
     // return a random item (but do not remove it)
-    public Item sample(){}
+    public Item sample(){
+        return null;
+    }
 
     // return an independent iterator over items in random order
-    public Iterator<Item> iterator(){}
+    public Iterator<Item> iterator(){
+        return null;
+    }
 
     // unit testing (required)
     public static void main(String[] args){}
